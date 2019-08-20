c728e8fa86150810968e37f5d5cd8a0abb4f9ec7
David Rifkin <50370157+davidlawrencer@users.noreply.github.com>
2019-08-20 15:36:21 -0400

    Update Contents.swift

--- Contents.swift
+++ Contents.swift
@@ -9,6 +9,7 @@ class Animal {
     }
 }
 
+//Define how animals are compared
 extension Animal: Equatable {
     static func == (lhs: Animal, rhs: Animal) -> Bool {
         return lhs.name == rhs.name
@@ -18,10 +19,13 @@ extension Animal: Equatable {
 let dog1 = Animal(name: "dog")
 let dog2 = dog1
 let dog3 = Animal(name: "dog")
+
+//equal to - calls static func called "=="
 dog1 == dog2
 dog1 == dog3
 dog2 == dog3
 
+//identity - checks reference location
 dog1 === dog2
 dog2 === dog3
 
@@ -32,16 +36,16 @@ class Buffalo {
     }
 }
 
-
-
 var anAnimal = Animal(name: "annette")
 anAnimal.name
 var aBuffalo = Buffalo(a: anAnimal)
 aBuffalo.animal.name
+
+//update property on delegate... still same reference
 aBuffalo.animal.name = "carl"
 aBuffalo.animal.name
 anAnimal.name
-
+//what happens if Animal and Buffalo are structs
 
 protocol Timeable {
     var runtime: Int {get set}
@@ -56,6 +60,7 @@ class TVSeries: Timeable {
 
 class Channel {
     var name: String
+    //channel delegates out its time slots to something that is timeable... could be a tv show, could be a live event, could be a movie
     var currentShow: Timeable?
     var howLongUntilNextShow: Int {
         get {
